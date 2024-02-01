5fea41be743b0b0bd0ec771ff27926c4ffc2491e
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2024-02-01 12:28:52 +1100

    added comments,added date between start date and stage date fixed loader position

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
     var timelineData = [
         {
-            "date": "01-01-2024",
+            "date": "01/01/2024",
             "description": "Slab poured",
+            "comments": "This is where the foundation was set.",
             "images": ["https://placehold.co/600x400/000000/FFF", "https://fastly.picsum.photos/id/418/200/300.jpg?hmac=T7cC_OCVJnIk98mcvhuKBWancCeGl2KcyuSBTCYE-QM"]
         },
         {
-            "date": "18-01-2024",
+            "date": "18/01/2024",
             "description": "Frame stage started",
+            "comments": "This is frame was set.",
             "images": ["https://placehold.co/600x400/000000/GGG", "https://fastly.picsum.photos/id/418/200/300.jpg?hmac=T7cC_OCVJnIk98mcvhuKBWancCeGl2KcyuSBTCYE-QM"]
         }
         // Add more events as needed
@@ -25,17 +27,41 @@ $(document).ready(function() {
         return galleryHtml;
     }
 
-    function displayTimelineEvents(data) {
+    function convertDateToISO(dateStr) {
+        const parts = dateStr.split('/');
+        return `${parts[2]}-${parts[1]}-${parts[0]}`;
+      }
+      
+      function calculateDaysSince(startDateStr, eventDateStr) {
+        const formattedStartDateStr = convertDateToISO(startDateStr);
+        const formattedEventDateStr = convertDateToISO(eventDateStr);
+      
+        const startDate = new Date(formattedStartDateStr);
+        const eventDate = new Date(formattedEventDateStr);
+        const timeDiff = eventDate - startDate;
+        const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        return daysDiff;
+      }
+      
+
+      function displayTimelineEvents(data) {
+        const startDateStr = "01/01/2022"; // Start date in DD/MM/YYYY format
+      
         var timelineHtml = '';
         data.forEach(function(event, index) {
-            timelineHtml += `<div class="max-w-xl w-full mx-auto bg-white p-5 rounded-lg shadow-md mb-8">`;
-            timelineHtml += `<div class="text-lg font-bold">${event.date}</div>`;
-            timelineHtml += `<p class="text-gray-700 my-2">${event.description}</p>`;
-            timelineHtml += createImageGallery(event, index);
-            timelineHtml += `</div>`;
+          const daysSinceStart = calculateDaysSince(startDateStr, event.date);
+      
+          timelineHtml += `<div class="max-w-xl w-full mx-auto bg-white p-5 rounded-lg shadow-md mb-8">`;
+          timelineHtml += `<div class="text-lg font-bold">${event.date} (${daysSinceStart} days since site start)</div>`;
+          timelineHtml += `<p class="text-gray-700 my-2">${event.description}</p>`;
+          timelineHtml += `<p class="text-gray-600">${event.comments}</p>`;
+          timelineHtml += createImageGallery(event, index);
+          timelineHtml += `</div>`;
         });
+      
         $('#events').html(timelineHtml);
-    }
+      }
+      
 
     $('#viewTimelineBtn').click(function() {
         $('#welcomeScreen').hide();
@@ -45,7 +71,7 @@ $(document).ready(function() {
         setTimeout(function() {
             $('#loader').hide();
             $('#timeline').removeClass('hidden');
-        }, 1500); // Adjust the time as needed (3000 ms = 3 seconds)
+        }, 500); // Adjust the time as needed (3000 ms = 3 seconds)
     });
 
     window.changeSlide = function(move, galleryIndex) {
